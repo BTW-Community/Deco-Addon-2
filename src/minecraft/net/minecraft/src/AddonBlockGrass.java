4d3e0a05cdfcae09ad3f0f54cebb9dee7c4f5e3a
ammoore00 <abigailmmoore00@gmail.com>
2020-07-19 15:38:28 -0600

    Fixed hemp seed bug

--- src/minecraft/net/minecraft/src/AddonBlockGrass.java
+++ src/minecraft/net/minecraft/src/AddonBlockGrass.java
@@ -8,6 +8,16 @@ public class AddonBlockGrass extends FCBlockGrass {
     public boolean ConvertBlock(ItemStack var1, World var2, int var3, int var4, int var5, int var6)
     {
         var2.setBlockWithNotify(var3, var4, var5, FCBetterThanWolves.fcBlockDirtLoose.blockID);
+
+        if (!var2.isRemote)
+        {
+            var2.playAuxSFX(2001, var3, var4, var5, this.blockID);
+
+            if (var2.rand.nextInt(25) == 0)
+            {
+                FCUtilsItem.EjectStackFromBlockTowardsFacing(var2, var3, var4, var5, new ItemStack(FCBetterThanWolves.fcItemHempSeeds), var6);
+            }
+        }
         
         return true;
     }
