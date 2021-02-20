84e346ae11c2f304e11d6d1669c2017a04807b0c
ammoore00 <abigailmmoore00@gmail.com>
2021-02-19 21:17:45 -0700

    Changed polished stone variants to drop loose cobble with less than diamond

--- src/minecraft/net/minecraft/src/DecoBlockStoneSmooth.java
+++ src/minecraft/net/minecraft/src/DecoBlockStoneSmooth.java
@@ -18,6 +18,36 @@ public class DecoBlockStoneSmooth extends Block{
 	{
 		return Meta;
 	}
+
+	public int GetHarvestToolLevel(IBlockAccess var1, int var2, int var3, int var4)
+	{
+		return 3;
+	}
+
+    public void OnBlockDestroyedWithImproperTool(World var1, EntityPlayer var2, int var3, int var4, int var5, int var6)
+    {
+        var1.playAuxSFX(2272, var3, var4, var5, this.blockID + (var6 << 12));
+        
+        ItemStack curItem = var2.getCurrentEquippedItem();
+        
+        if (curItem != null && (curItem.itemID == Item.pickaxeStone.itemID || curItem.itemID == Item.pickaxeIron.itemID)) {
+        	this.DropComponentItemsOnBadBreak(var1, var3, var4, var5, var6, 1.0F);
+        }
+    }
+
+	public boolean DropComponentItemsOnBadBreak(World var1, int var2, int var3, int var4, int var5, float var6)
+	{
+		if (var5 == 0) {
+			this.DropItemsIndividualy(var1, var2, var3, var4, DecoDefs.graniteCobbleLoose.blockID, 1, 0, var6);
+		}
+		else if (var5 == 1) {
+			this.DropItemsIndividualy(var1, var2, var3, var4, DecoDefs.andesiteCobbleLoose.blockID, 1, 0, var6);
+		}
+		else if (var5 == 2) {
+			this.DropItemsIndividualy(var1, var2, var3, var4, DecoDefs.dioriteCobbleLoose.blockID, 1, 0, var6);
+		}
+		return true;
+	}
 	
 	//CLIENT ONLY METHODS
 	public static Icon[] Icons = new Icon[3];
